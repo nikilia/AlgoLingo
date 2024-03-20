ad67d8ccbc866b28219e83fc09bfec046689dff8
Niki <63308844+nikilia@users.noreply.github.com>
2024-03-20 17:50:44 +0100

    fixed wrong path

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // init sqlite db
-const dbFile = "./.data/sqlite.db";
+const dbFile = "./sqlite.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
